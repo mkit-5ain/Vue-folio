f07868a6987dd74d279b97be8fd5b253375c84d7
mkit-5ain <mkit5ain@gmail.com>
2022-01-25 18:06:37 +0900

    [update] 불필요한 요소 삭제

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,145 +1,147 @@
 <template>
-    <section class="circle component__style">
-        <img src="@/assets/image/common/circle.svg" alt="">
-    </section>
-    <section class="work component__style">
-        <ui-title/>
-        <div class="item t1 scroll-handler">
-            <div class="image">
-                <img src="@/assets/image/common/project/s_3.png" alt="">
-                <p class="image__name">Design - Skeuomorphism / World</p>
+    <section id="home">
+        <ui-header/>
+        <section class="circle component__style">
+            <img src="@/assets/image/common/circle.svg" alt="">
+        </section>
+        <section class="work component__style">
+            <ui-title/>
+            <div class="item t1 scroll-handler">
+                <div class="image">
+                    <div class="image__inner"><img src="@/assets/image/common/project/s_3.png" alt=""></div>
+                    <p class="image__name">Design - Skeuomorphism / World</p>
+                </div>
+                <div class="discription">
+                    <p><span>From Romania Ali moved to</span></p>
+                    <p><span>Germany with Leagas</span></p>
+                    <p><span>Delaney, Hamburg. Where he</span></p>
+                    <p><span>worked under a great Art</span></p>
+                    <p><span>Director, Hermann</span></p>
+                    <p><span>Waterkamp. From there he</span></p>
+                    <p><span>went on to build a truly global</span></p>
+                    <p><span>career, first as Art Director</span></p>
+                    <p><span>then as Creative Director for</span></p>
+                    <p><span>the likes of Leo Burnett, JWT,</span></p>
+                    <p><span>McCann, before finally</span></p>
+                    <p><span>topping it off as ECD of</span></p>
+                    <p><span>DDB-Berlin in 2012.</span></p>
+                </div>
             </div>
-            <div class="discription">
-                <p><span>From Romania Ali moved to</span></p>
-                <p><span>Germany with Leagas</span></p>
-                <p><span>Delaney, Hamburg. Where he</span></p>
-                <p><span>worked under a great Art</span></p>
-                <p><span>Director, Hermann</span></p>
-                <p><span>Waterkamp. From there he</span></p>
-                <p><span>went on to build a truly global</span></p>
-                <p><span>career, first as Art Director</span></p>
-                <p><span>then as Creative Director for</span></p>
-                <p><span>the likes of Leo Burnett, JWT,</span></p>
-                <p><span>McCann, before finally</span></p>
-                <p><span>topping it off as ECD of</span></p>
-                <p><span>DDB-Berlin in 2012.</span></p>
+            <div class="item t2 scroll-handler">
+                <div class="image">
+                    <div class="image__inner"><img src="@/assets/image/common/project/s_2.png" alt=""></div>
+                    <p class="image__name">Design - Newmorphism / World</p>
+                </div>
+                <div class="discription">
+                    <p><span>It may not be obvious, but Ali</span></p>
+                    <p><span>studied computer</span></p>
+                    <p><span>engineering at the American</span></p>
+                    <p><span>University in Cairo, with a</span></p>
+                    <p><span>minor in painting. Preferring</span></p>
+                    <p><span>storyboards to motherboards,</span></p>
+                    <p><span>he quickly abandoned a</span></p>
+                    <p><span>mediocre programming</span></p>
+                    <p><span>career to study Design and</span></p>
+                    <p><span>Art Direction at the</span></p>
+                    <p><span>Miami Ad School.</span></p>
+                </div>
             </div>
-        </div>
-        <div class="item t2 scroll-handler">
-            <div class="image">
-                <img src="@/assets/image/common/project/s_2.png" alt="">
-                <p class="image__name">Design - Newmorphism / World</p>
+            <div class="item t3 scroll-handler">
+                <div class="image">
+                    <div class="image__inner"><img src="@/assets/image/common/project/s_4.png" alt=""></div>
+                    <p class="image__name">Design - Hyundai / Korea</p>
+                </div>
+                <div class="discription">
+                    <p><span>Ali spent a little over a year in</span></p>
+                    <p><span>Miami before moving to</span></p>
+                    <p><span>Romania as a Junior Art</span></p>
+                    <p><span>Director for D'Arcy Bucharest.</span></p>
+                    <p><span>He met some incredible</span></p>
+                    <p><span>people at the now-closed</span></p>
+                    <p><span>Agency, and they’re still</span></p>
+                    <p><span>friends to this day.</span></p>
+                </div>
             </div>
-            <div class="discription">
-                <p><span>It may not be obvious, but Ali</span></p>
-                <p><span>studied computer</span></p>
-                <p><span>engineering at the American</span></p>
-                <p><span>University in Cairo, with a</span></p>
-                <p><span>minor in painting. Preferring</span></p>
-                <p><span>storyboards to motherboards,</span></p>
-                <p><span>he quickly abandoned a</span></p>
-                <p><span>mediocre programming</span></p>
-                <p><span>career to study Design and</span></p>
-                <p><span>Art Direction at the</span></p>
-                <p><span>Miami Ad School.</span></p>
+            <div class="item t1 scroll-handler">
+                <div class="image">
+                    <div class="image__inner"><img src="@/assets/image/common/project/s_3.png" alt=""></div>
+                    <p class="image__name">Design - Skeuomorphism / World</p>
+                </div>
+                <div class="discription">
+                    <p><span>From Romania Ali moved to</span></p>
+                    <p><span>Germany with Leagas</span></p>
+                    <p><span>Delaney, Hamburg. Where he</span></p>
+                    <p><span>worked under a great Art</span></p>
+                    <p><span>Director, Hermann</span></p>
+                    <p><span>Waterkamp. From there he</span></p>
+                    <p><span>went on to build a truly global</span></p>
+                    <p><span>career, first as Art Director</span></p>
+                    <p><span>then as Creative Director for</span></p>
+                    <p><span>the likes of Leo Burnett, JWT,</span></p>
+                    <p><span>McCann, before finally</span></p>
+                    <p><span>topping it off as ECD of</span></p>
+                    <p><span>DDB-Berlin in 2012.</span></p>
+                </div>
             </div>
-        </div>
-        <div class="item t3 scroll-handler">
-            <div class="image">
-                <img src="@/assets/image/common/project/s_4.png" alt="">
-                <p class="image__name">Design - Hyundai / Korea</p>
+            <div class="item t2 scroll-handler">
+                <div class="image">
+                    <div class="image__inner"><img src="@/assets/image/common/project/s_2.png" alt=""></div>
+                    <p class="image__name">Design - Newmorphism / World</p>
+                </div>
+                <div class="discription">
+                    <p><span>It may not be obvious, but Ali</span></p>
+                    <p><span>studied computer</span></p>
+                    <p><span>engineering at the American</span></p>
+                    <p><span>University in Cairo, with a</span></p>
+                    <p><span>minor in painting. Preferring</span></p>
+                    <p><span>storyboards to motherboards,</span></p>
+                    <p><span>he quickly abandoned a</span></p>
+                    <p><span>mediocre programming</span></p>
+                    <p><span>career to study Design and</span></p>
+                    <p><span>Art Direction at the</span></p>
+                    <p><span>Miami Ad School.</span></p>
+                </div>
             </div>
-            <div class="discription">
-                <p><span>Ali spent a little over a year in</span></p>
-                <p><span>Miami before moving to</span></p>
-                <p><span>Romania as a Junior Art</span></p>
-                <p><span>Director for D'Arcy Bucharest.</span></p>
-                <p><span>He met some incredible</span></p>
-                <p><span>people at the now-closed</span></p>
-                <p><span>Agency, and they’re still</span></p>
-                <p><span>friends to this day.</span></p>
+            <div class="item t3 scroll-handler">
+                <div class="image">
+                    <div class="image__inner"><img src="@/assets/image/common/project/s_4.png" alt=""></div>
+                    <p class="image__name">Design - Hyundai / Korea</p>
+                </div>
+                <div class="discription">
+                    <p><span>Ali spent a little over a year in</span></p>
+                    <p><span>Miami before moving to</span></p>
+                    <p><span>Romania as a Junior Art</span></p>
+                    <p><span>Director for D'Arcy Bucharest.</span></p>
+                    <p><span>He met some incredible</span></p>
+                    <p><span>people at the now-closed</span></p>
+                    <p><span>Agency, and they’re still</span></p>
+                    <p><span>friends to this day.</span></p>
+                </div>
             </div>
-        </div>
-        <div class="item t1 scroll-handler">
-            <div class="image">
-                <img src="@/assets/image/common/project/s_3.png" alt="">
-                <p class="image__name">Design - Skeuomorphism / World</p>
+        </section>
+        <section class="story scroll-handler">
+            <p><span>NAMED BY THE</span></p>
+            <p><span>2014 GUNN REPORT®</span></p>
+            <p><span>AS ONE OF THE TOP TEN</span></p>
+            <p><span>DIRECTORS IN THE WORLD.</span></p>
+        </section>
+        <section class="age component__style scroll-handler">
+            <ui-title/>
+            <div class="inner">
+                <div class="front">
+                    <span>2</span>
+                    <span>3</span>
+                </div>
+                <div class="back">
+                    <span>5</span>
+                    <span>4</span>
+                    <span>3</span>
+                    <span>2</span>
+                </div>
+                <div class="text">AGE</div>
             </div>
-            <div class="discription">
-                <p><span>From Romania Ali moved to</span></p>
-                <p><span>Germany with Leagas</span></p>
-                <p><span>Delaney, Hamburg. Where he</span></p>
-                <p><span>worked under a great Art</span></p>
-                <p><span>Director, Hermann</span></p>
-                <p><span>Waterkamp. From there he</span></p>
-                <p><span>went on to build a truly global</span></p>
-                <p><span>career, first as Art Director</span></p>
-                <p><span>then as Creative Director for</span></p>
-                <p><span>the likes of Leo Burnett, JWT,</span></p>
-                <p><span>McCann, before finally</span></p>
-                <p><span>topping it off as ECD of</span></p>
-                <p><span>DDB-Berlin in 2012.</span></p>
-            </div>
-        </div>
-        <div class="item t2 scroll-handler">
-            <div class="image">
-                <img src="@/assets/image/common/project/s_2.png" alt="">
-                <p class="image__name">Design - Newmorphism / World</p>
-            </div>
-            <div class="discription">
-                <p><span>It may not be obvious, but Ali</span></p>
-                <p><span>studied computer</span></p>
-                <p><span>engineering at the American</span></p>
-                <p><span>University in Cairo, with a</span></p>
-                <p><span>minor in painting. Preferring</span></p>
-                <p><span>storyboards to motherboards,</span></p>
-                <p><span>he quickly abandoned a</span></p>
-                <p><span>mediocre programming</span></p>
-                <p><span>career to study Design and</span></p>
-                <p><span>Art Direction at the</span></p>
-                <p><span>Miami Ad School.</span></p>
-            </div>
-        </div>
-        <div class="item t3 scroll-handler">
-            <div class="image">
-                <img src="@/assets/image/common/project/s_4.png" alt="">
-                <p class="image__name">Design - Hyundai / Korea</p>
-            </div>
-            <div class="discription">
-                <p><span>Ali spent a little over a year in</span></p>
-                <p><span>Miami before moving to</span></p>
-                <p><span>Romania as a Junior Art</span></p>
-                <p><span>Director for D'Arcy Bucharest.</span></p>
-                <p><span>He met some incredible</span></p>
-                <p><span>people at the now-closed</span></p>
-                <p><span>Agency, and they’re still</span></p>
-                <p><span>friends to this day.</span></p>
-            </div>
-        </div>
-    </section>
-    <section class="story scroll-handler">
-        <p><span>NAMED BY THE</span></p>
-        <p><span>2014 GUNN REPORT®</span></p>
-        <p><span>AS ONE OF THE TOP TEN</span></p>
-        <p><span>DIRECTORS IN THE WORLD.</span></p>
-    </section>
-    <section class="age component__style scroll-handler">
-        <ui-title/>
-        <div class="inner">
-            <div class="front">
-                <span>2</span>
-                <span>3</span>
-            </div>
-            <div class="back">
-                <span>5</span>
-                <span>4</span>
-                <span>3</span>
-                <span>2</span>
-            </div>
-            <div class="text">AGE</div>
-        </div>
-    </section>
-    <section class="contact component__style scroll-handler">
+        </section>
+        <section class="contact component__style scroll-handler">
         <ui-title/>
         <div class="inner">
             <div class="sub-title">
@@ -147,18 +149,20 @@
                 <p><span>In</span></p>
                 <p><span>Touch</span></p>
             </div>
-            <p class="item"><span>Welcome To This Place</span></p>
-            <p class="item"><span>Trap House</span></p>
-            <p class="item"><span>ManoverBoard</span></p>
-            <p class="item"><span>Outhere Music</span></p>
-            <p class="item"><span>Paris In the Rain</span></p>
-            <p class="item"><span>My Universe</span></p>
-            <p class="item"><span>Creative Practices</span></p>
+            <p class="item scale"><span>Welcome To This Place</span></p>
+            <p class="item scale"><span>Trap House</span></p>
+            <p class="item scale"><span>ManoverBoard</span></p>
+            <p class="item scale"><span>Outhere Music</span></p>
+            <p class="item scale"><span>Paris In the Rain</span></p>
+            <p class="item scale"><span>My Universe</span></p>
+            <p class="item scale"><span>Creative Practices</span></p>
         </div>
+    </section>
     </section>
 </template>
 
 <script>
+    import Header from '@/components/Header.vue';
     import Title from '@/components/Title.vue';
     import gsap from 'gsap';
     import ScrollTrigger from "gsap/ScrollTrigger";
@@ -167,13 +171,14 @@
     export default {
         name: 'Home',
         components: {
+            'ui-header': Header,
             'ui-title': Title
         },
         mounted:function () {
             window.addEventListener("scroll", this.sectionHandle); // 스크롤 섹션 접근시 scroll-handler에 클래스추가
             window.addEventListener("scroll", this.bgHandle); // 스크롤 섹션 접근시 body에 클래스 추가
             this.horizontalScroll(); // 가로 스크롤
-            //this.textCrossAnimation(); // 텍스트 교차 애니메이션
+            //this.timeAnimation(); // 텍스트 교차 애니메이션
         },
         methods: {
             sectionHandle: function () {
@@ -226,26 +231,17 @@
                     });
                 });
             },
-            textCrossAnimation: function () {
-                gsap.to(".left--direction", {
-                    xPercent: 20,
+            timeAnimation: function () {
+                gsap.to(".time-difference", {
+                    yPercent: -50,
                     ease: "none",
                     scrollTrigger: {
-                        trigger: ".story",
-                        scrub: 3,
+                        trigger: ".work",
+                        scrub: 1,
                         start: "top center"
                     },
                 });
 
-                gsap.to(".right--direction", {
-                    xPercent: -20,
-                    ease: "none",
-                    scrollTrigger: {
-                        trigger: ".appeal",
-                        scrub: 3,
-                        start: "top center"
-                    },
-                });
             }
         }
     }
