5685c16de050d4dfe58cfd34a0dfd3e0fd88ee14
mkit-5ain <mkit5ain@gmail.com>
2022-01-25 19:00:26 +0900

    [update] router modify

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -28,8 +28,11 @@ const routes = [
 ]
 
 const router = createRouter({
+    routes,
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    scrollBehavior() {
+        return { top: 0 }
+    },
 })
 
 export default router
