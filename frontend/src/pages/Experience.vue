d8d7ddc6a0adb0ef721bb9d3c04201af15c0d7af
mkit-5ain <mkit5ain@gmail.com>
2022-01-27 17:37:41 +0900

    [update] 페이지 레이아웃 작업

--- frontend/src/pages/Experience.vue
+++ frontend/src/pages/Experience.vue
@@ -1,7 +1,26 @@
 <template>
     <section id="exp">
         <ui-prologue/>
-        <section class="component__style"></section>
+        <section class="work__list component__style">
+            <ul class="list__wrap">
+                <li class="item">1</li>
+                <li class="item">2</li>
+                <li class="item">3</li>
+                <li class="item">4</li>
+            </ul>
+            <ul class="list__wrap">
+                <li class="item">5</li>
+                <li class="item">6</li>
+                <li class="item">7</li>
+            </ul>
+            <ul class="list__wrap">
+                <li class="item">8</li>
+                <li class="item">9</li>
+                <li class="item">10</li>
+                <li class="item">9</li>
+                <li class="item">10</li>
+            </ul>
+        </section>
     </section>
 </template>
 
@@ -15,5 +34,11 @@
         }
     }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+    .work__list {
+        .list__wrap { display: flex;
+            .item { border-bottom: 1px solid #111; border-right: 1px solid #111; }
+        }
+    }
+
 </style>
