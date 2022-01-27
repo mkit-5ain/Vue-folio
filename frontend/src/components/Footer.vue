43faf823069b61707fb2d8d32699beed577c536c
mkit-5ain <mkit5ain@gmail.com>
2022-01-27 17:37:15 +0900

    [update] 하단 푸터 러프하게 레이아웃 작업

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,21 +1,74 @@
 <template>
-    <section id="footer" class="footer">
-        <p>All Rights Reserved © 2022</p>
-        <p>Back To Top</p>
+    <section class="footer--sticky">
+        <div class="inner">
+            <p>All Rights Reserved © 2022</p>
+            <p class="scale scroll--top">Back To Top</p>
+        </div>
+    </section>
+    <section class="footer">
+        <div class="widget">
+            <p><span>L</span>ike</p>
+            <p><span>I</span>deas</p>
+            <p><span>M</span>ake</p>
+        </div>
+        <div class="widget">
+            <p><span>J</span>oin</p>
+            <p><span>U</span>sually</p>
+            <p><span>N</span>otion</p>
+            <p><span>E</span>ver</p>
+        </div>
+        <div class="widget">
+            <p><span>S</span>hiny</p>
+            <p><span>U</span>nderline</p>
+        </div>
     </section>
 </template>
 
 <script>
-    export default {}
+    export default {
+        name: 'Footer',
+        mounted:function () {
+            this.scrollTop();
+        },
+        methods: {
+            scrollTop: function () {
+                let element = document.getElementsByClassName('scroll--top');
+                element[0].addEventListener('click', ()=> {
+                    window.scrollTo(0,0);
+                });
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-    #footer {
-        display: flex;
+    .footer--sticky {
         position: sticky;
         bottom: 1vw;
         margin: 4vw 6vw;
         font-weight: bold;
-        justify-content:  space-between;
+        .inner { display: flex; justify-content: space-between; }
+    }
+    .footer {
+        display: flex;
+        position: relative;
+        margin: 0 6vw 6vw;
+        justify-content: space-between;
+        .widget {
+            p { font-weight: bold;
+                span { position: relative; font-size: 20px;
+                    &:before {
+                        content: '';
+                        display: block;
+                        position: absolute;
+                        bottom: 0px;
+                        left: 0px;
+                        width: 100%;
+                        height: 1px;
+                        border-bottom: 1px solid #1d1d1b;
+                    }
+                }
+            }
+        }
     }
 </style>
